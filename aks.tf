20b77b26a644c4464a5cee2b44d343a1a1994a92
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2020-09-09 09:38:41 -0300

    Adding subnet

--- aks.tf
+++ aks.tf
@@ -3,6 +3,15 @@ resource "azurerm_resource_group" "aks_resource_group" {
   location = var.location
 }
 
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.aks_resource_group.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+  depends_on          = [azurerm_resource_group.aks_resource_group]
+}
+
 module "aks" {
   source              = "Azure/aks/azurerm"
   resource_group_name = azurerm_resource_group.aks_resource_group.name
